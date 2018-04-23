1aed220fb37b4f7cf7dbc4d7b9736bb0b9ff4744
Ronak Jain <ronak.jain2116@gmail.com>
2018-04-23 23:58:42 +0530

    fulcrum modification

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -18,7 +18,6 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :letter_opener
   config.action_mailer.default_url_options = { :host => config.fulcrum.app_host }
   
-
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
