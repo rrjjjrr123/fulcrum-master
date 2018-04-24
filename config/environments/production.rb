6add2b288d8bbc31b768c4202f861bfdf037a38d
Ronak Jain <ronak.jain2116@gmail.com>
2018-04-24 14:05:45 +0530

    modfify

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,19 +59,18 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.raise_delivery_errors = true
-config.action_mailer.delivery_method = :smtp
+config.action_mailer.raise_delivery_errors = true
+
 config.action_mailer.perform_deliveries = true
 
 I
-config.action_mailer.default_url_options = {:host => 'fulcrumproject.herokuapp.com', :protocol => 'http'}
+
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
 
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
-
   # Disable automatic flushing of the log to improve performance.
   # config.autoflush_log = false
  
