7cf765bb53c9fd8bf025b4b981c915cc291364f5
Ronak Jain <ronak.jain2116@gmail.com>
2018-04-24 22:10:43 +0530

    modify

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -18,7 +18,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = { :host => config.fulcrum.app_host }
   confi.action_mailer.smtp_settings = {
-    address: "smtp.gmail.com"
+    address: "smtp.gmail.com",
     port: 587,
     domain: "railscasts.com",
     authentication: "plain",
